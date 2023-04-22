be2267e47921313e9eac07fc529432b3f149087b
Filipe Haniel <filipe.hanielloper@gmail.com>
2023-04-22 17:21:52 -0300

    initializing modular structure

---
+++ lib/app/app_module.dart
@@ -0,0 +1,12 @@
+import 'package:colored_diet/app/modules/splash/splash_page.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  @override
+  List<Bind> get binds => [];
+
+  @override
+  List<ModularRoute> get routes => [
+        ChildRoute('/', child: (context, args) => const SplashPage()),
+      ];
+}
